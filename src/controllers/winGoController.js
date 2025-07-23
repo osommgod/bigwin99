4a3794ebf91863ed8fffbff634c4bf6110117fe9
OMM KALYAN <ommkalyan3@gmail.com>
2025-07-23 14:21:11 +0530

    latset_push_23_7_25

--- src/controllers/winGoController.js
+++ src/controllers/winGoController.js
@@ -392,7 +392,7 @@ const betWinGo = async (req, res) => {
         money = ?,
         amount = ?,
         fee = ?,
-        get = ?,
+        \`get\` = ?,
         game = ?,
         bet = ?,
         status = ?,
